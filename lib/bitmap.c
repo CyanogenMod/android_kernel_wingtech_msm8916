b8f61adca670b802796008fbeeba631a30943c70
Pan Xinhui <xinhuix.pan@intel.com>
2015-09-09 15:37:05 -0700

    lib/bitmap.c: fix a special string handling bug in __bitmap_parselist
    If string end with '-', for exapmle, bitmap_parselist("1,0-",&mask,
    nmaskbits), It is not in a valid pattern, so add a check after loop.
    Return -EINVAL on such condition.
    
    Signed-off-by: Pan Xinhui <xinhuix.pan@intel.com>
    Cc: Yury Norov <yury.norov@gmail.com>
    Cc: Chris Metcalf <cmetcalf@ezchip.com>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- lib/bitmap.c
+++ lib/bitmap.c
@@ -644,7 +644,7 @@ static int __bitmap_parselist(const char *buf, unsigned int buflen,
 					return -EINVAL;
 				b = 0;
 				in_range = 1;
-				exp_digit = 1;
+				at_start = 1;
 				continue;
 			}
 
@@ -657,6 +657,9 @@ static int __bitmap_parselist(const char *buf, unsigned int buflen,
 			exp_digit = 0;
 			totaldigits++;
 		}
+		/* if no digit is after '-', it's wrong*/
+		if (at_start && in_range)
+			return -EINVAL;
 		if (!(a <= b))
 			return -EINVAL;
 		if (b >= nmaskbits)
