c1f1e6bc3729ac51df19ea2dfaa8722899ca8262
Hemant Sharma <hemantbeast@gmail.com>
2016-01-05 10:36:27 +0530

    fix ft5x06
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>
    
    Conflicts:
    	drivers/input/touchscreen/ft5x06_ts.c
    
    Change-Id: I7ebf1c1944b2c868ca691f228ae78c41504ff352

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -1369,11 +1369,14 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 			ft5x06_data && ft5x06_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK) {
-			if (unblanked_once)
+		if (*blank == FB_BLANK_UNBLANK || (*blank == FB_BLANK_VSYNC_SUSPEND)) {
+			if (unblanked_once) {
+				pr_info("ft5x06 resume!\n");
 				ft5x06_ts_resume(&ft5x06_data->client->dev);
+			}
 		} else if (*blank == FB_BLANK_POWERDOWN) {
 			unblanked_once = true;
+			pr_info("ft5x06 suspend!\n");
 			ft5x06_ts_suspend(&ft5x06_data->client->dev);
 		}
 	}
