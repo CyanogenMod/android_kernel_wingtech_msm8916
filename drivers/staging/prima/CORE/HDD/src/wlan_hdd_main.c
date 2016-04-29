191bca3b4d1c1fe68b8fe60080e6f56956539df6
James Christopher Adduono <jc@adduono.com>
2016-04-29 04:15:22 -0400

    Prevent panic reboot on disconnect of wlan1

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -8825,9 +8825,9 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
    unregister_inetaddr_notifier(&pHddCtx->ipv4_notifier);
 
    // Unregister the Net Device Notifier
-   unregister_netdevice_notifier(&hdd_netdev_notifier);
+   //unregister_netdevice_notifier(&hdd_netdev_notifier);
    
-   hdd_stop_all_adapters( pHddCtx );
+   //hdd_stop_all_adapters( pHddCtx );
 
 #ifdef WLAN_BTAMP_FEATURE
    vosStatus = WLANBAP_Stop(pVosContext);
@@ -10304,6 +10304,7 @@ int hdd_wlan_startup(struct device *dev )
    }
 
    // register net device notifier for device change notification
+/*
    ret = register_netdevice_notifier(&hdd_netdev_notifier);
 
    if(ret < 0)
@@ -10311,12 +10312,13 @@ int hdd_wlan_startup(struct device *dev )
       hddLog(VOS_TRACE_LEVEL_ERROR,"%s: register_netdevice_notifier failed",__func__);
       goto err_unregister_pmops;
    }
+*/
 
    //Initialize the nlink service
    if(nl_srv_init() != 0)
    {
       hddLog(VOS_TRACE_LEVEL_FATAL,"%s: nl_srv_init failed", __func__);
-      goto err_reg_netdev;
+      goto err_nl_srv;
    }
 
 #ifdef WLAN_KD_READY_NOTIFIER
@@ -10479,8 +10481,8 @@ err_nl_srv:
 #else
    nl_srv_exit();
 #endif /* WLAN_KD_READY_NOTIFIER */
-err_reg_netdev:
-   unregister_netdevice_notifier(&hdd_netdev_notifier);
+//err_reg_netdev:
+//   unregister_netdevice_notifier(&hdd_netdev_notifier);
 
 err_unregister_pmops:
    hddDevTmUnregisterNotifyCallback(pHddCtx);
