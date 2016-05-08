e9c4230ac195b113f62073439d345f73c35c86ac
guts94 <im.ashish994@gmail.com>
2016-05-08 11:46:41 +0530

    cpufreq: add adaptive and darkness governors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -453,6 +453,9 @@ extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
 extern struct cpufreq_governor cpufreq_gov_zzmoove;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_zzmoove)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
+extern struct cpufreq_governor cpufreq_gov_darkness;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
 #endif
 
 /*********************************************************************
