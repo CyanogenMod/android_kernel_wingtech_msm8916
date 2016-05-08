87687b1984853e4bd53f7d4a8c61fdfddc7f9d28
ZaneZam <cyxman@yahoo.com>
2016-05-08 11:39:38 +0530

    cpufreq: add zzmoove governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -450,6 +450,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
